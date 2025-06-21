a23e3734cd7b4ab7919c07a73819da0ad404eb73
kk <kshitijz1x2@gmail.com>
2025-06-21 06:42:46 +0000

    try mlflow ns

---
+++ dags/train_model_k8s_mlflow_ns.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from datetime import datetime
+
+with DAG(dag_id="train_model_on_k8s",
+         start_date=datetime(2024, 1, 1),
+         schedule_interval=None,
+         catchup=False) as dag:
+
+    train_model = KubernetesPodOperator(
+        task_id="train-model",
+        name="train-model",
+        namespace="mlflow",
+        image="mnist-train-mlflow:latest",
+        is_delete_operator_pod=True,
+        get_logs=True,
+        image_pull_policy="Never"
+)
