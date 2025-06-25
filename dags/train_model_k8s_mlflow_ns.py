cc6ac0ff48096f83857864799a7e99c711b03ff2
Kshitij <kshitij@example.com>
2025-06-25 17:24:35 +0000

    correct dag af>2.9

--- dags/train_model_k8s_mlflow_ns.py
+++ dags/train_model_k8s_mlflow_ns.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 with DAG(dag_id="train_model_on_k8s_mlflow_ns",
          start_date=datetime(2024, 1, 1),
-         schedule_interval=None,
+         schedule=None,
          catchup=False) as dag:
 
     train_model = KubernetesPodOperator(
